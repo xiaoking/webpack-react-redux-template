4694267b45851fcba3acaf3cd80568aa3e816c08
ainiderenxm <277407493@qq.com>
2016-07-25 14:33:09 +0800

    add enties

--- src/entries/msg.jsx
+++ src/entries/msg.jsx
@@ -5,12 +5,12 @@ import React, { Component ,PropTypes} from 'react';
 import {render} from "react-dom";
 import {BindReact} from 'eg-tools';
 
-import Index from '../containers/web/Index.jsx';
+import Msg from '../containers/web/Msg.jsx';
 import * as reducers from '../reducers/index.es6';
 
 //判断执行dev环境
 
 render(
-    <BindReact Module={Index} reducers={reducers} autoShowFetching={true} barName="web" />,
+    <BindReact Module={Msg} reducers={reducers} autoShowFetching={true} barName="web" />,
     document.getElementById('root')
 );
