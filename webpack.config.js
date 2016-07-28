0531d8220633fdefc336024a2da4f19893a86c85
ainiderenxm <277407493@qq.com>
2016-07-28 11:43:42 +0800

    m

--- webpack.config.js
+++ webpack.config.js
@@ -147,8 +147,9 @@ var webpackConfig = {
         }),
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.CommonsChunkPlugin({
-            name: "common",
-            filename: "common.js",
+            //name: "common",
+            //filename: "common.js",
+            names: setCommonsChuck(),
             minChunks: Infinity
 
         })
